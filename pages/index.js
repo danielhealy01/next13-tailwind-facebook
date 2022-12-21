13b31cfafc280e7e6c14fef968aa3ddf345a695d
DanielHealy <danielhealyhosting3@gmail.com>
2022-12-21 21:18:59 +0000

    more

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
-import Card from '../components/Card'
+import Avatar from "../components/Avatar";
+import Card from "../components/Card";
 import Navigation from "../components/Navigation";
-import PostFormCard from '../components/PostFormCard';
+import PostCard from "../components/PostCard";
+import PostFormCard from "../components/PostFormCard";
 
 export default function Home() {
   return (
     <div className="flex mt-4 max-w-4xl mx-auto gap-6">
-      <div className="w-1/3">
+      <div className="w-3/12">
         <Navigation />
       </div>
-      <div className="grow">
+      <div className="w-9/12">
         <PostFormCard />
-        <Card>first post test</Card>
+        <PostCard />
       </div>
     </div>
   );
