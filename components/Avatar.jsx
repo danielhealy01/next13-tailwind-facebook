8d19208b8613179bc7b5d9ba6131ecd52d6b5887
DanielHealy <danielhealyhosting3@gmail.com>
2022-12-21 00:31:43 +0000

    Navigation and Post Submit Form Skeletons

---
+++ components/Avatar.jsx
@@ -0,0 +1,9 @@
+function Avatar() {
+  return (
+    <div className="w-12 rounded-full overflow-hidden">
+      <img src="../1516856346853.jpeg" alt="avatar" />
+    </div>
+  );
+}
+
+export default Avatar;
