dfeaadc81e1d1abd308cbfa6351d100f2fce8b89
DanielHealy <danielhealyhosting3@gmail.com>
2022-12-21 21:58:13 +0000

    Post menu  skeleton

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,6 +1,6 @@
 import Avatar from "./Avatar";
 import Card from "./Card";
-import { useState } from 'react'
+import { useState } from "react";
 
 function PostCard() {
   const [dropdownMenuOpen, setDropdownMenuOpen] = useState(false);
@@ -18,7 +18,10 @@ function PostCard() {
           </p>
           <p className="text-gray-500 text-sm">2 hours ago</p>
         </div>
-        <button className="text-gray-400">
+        <button
+          className="text-gray-400"
+          onClick={() => setDropdownMenuOpen(!dropdownMenuOpen)}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -39,6 +42,97 @@ function PostCard() {
           {dropdownMenuOpen && <div>dropdown menu</div>}
         </ClickOutHandler>
          If above is deleted, works. had to --force clickouthandler npm i as no support 18 */}
+        <div className="relative">
+          {dropdownMenuOpen && (
+            <div className="absolute right-2 top-8 bg-white shadow-md shadow-gray-300 p-3 rounded-sm w-52">
+              <a className="flex gap-2 p-2" href="">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+                  />
+                </svg>
+                Save post
+              </a>
+              <a className="flex gap-2 p-2" href="">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+                  />
+                </svg>
+                Stop notifications
+              </a>
+              <a className="flex gap-2 p-2" href="">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+                Hide posts
+              </a>
+              <a className="flex gap-2 p-2" href="">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                  />
+                </svg>
+                Delete
+              </a>
+              <a className="flex gap-2 p-2" href="">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+                  />
+                </svg>
+                Report
+              </a>
+            </div>
+          )}
+        </div>
       </div>
       <div>
         <p className="my-3 text-sm">
